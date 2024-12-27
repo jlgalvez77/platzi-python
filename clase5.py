a139159765e5642aefcda444be736cca1c6c4340
Jose Galvez <jlgalvez77@gmail.com>
2024-12-27 22:41:39 +0100

    clase5

---
+++ clase5.py
@@ -0,0 +1,25 @@
+x = 10
+y = 5.678
+z = 1.2e6
+a = 1.2e-6
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(x)
+print(y)
+print(z)
+print(a)
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)
+print(x // y)
+print(x % y)
+is_true = True
+is_false = False
+print(type(is_true))
+print(type(is_false))
+print(is_true)
+print(is_false)
