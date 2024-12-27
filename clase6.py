721889902445e4f414eb674886e62c9f8772e2bc
Jose Galvez <jlgalvez77@gmail.com>
2024-12-27 22:58:38 +0100

    clase6

---
+++ clase6.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+print(f'Suma: {a + b}')
+print(f'Resta: {a - b}')
+print(f'Multiplicación: {a * b}')
+print(f'División: {a / b}')
+
+print(f'Módulo: {a % b}')
+print(f'División entera: {a // b}')
+print(f'Potencia: {a ** b}')
+
+a += 1
+print(a)
+
+operation1 = 2 + 3 * 4
+operation2 = (2 + 3) * 4
+print(operation1)
+print(operation2)
